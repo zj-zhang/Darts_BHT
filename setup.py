d30b29d6428bb392ece397a631888f9144cedc9e
zj-zhang <zzj.zju@gmail.com>
2019-02-26 14:52:29 -0800

    test rtd by conditioning rpy2

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-
+import os
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
 
 def main():
 	setup(
@@ -27,21 +28,21 @@ def main():
 			'm2r',   # for doc build
 			]
 		 )
+	if not on_rtd:
+		from rpy2.robjects.packages import importr
+		utils = importr('utils')
+		utils.chooseCRANmirror(ind=1) 
+		R_dependency_list = ['Rcpp', 'doSNOW', 'getopt', 'mixtools', 'ggplot2']
+		from rpy2.robjects.vectors import StrVector
+		import rpy2.robjects.packages as rpackages
+		utils.chooseCRANmirror(ind=1) # select the first mirror in the list
 
-	from rpy2.robjects.packages import importr
-	utils = importr('utils')
-	utils.chooseCRANmirror(ind=1) 
-	R_dependency_list = ['Rcpp', 'doSNOW', 'getopt', 'mixtools', 'ggplot2']
-	from rpy2.robjects.vectors import StrVector
-	import rpy2.robjects.packages as rpackages
-	utils.chooseCRANmirror(ind=1) # select the first mirror in the list
+		# Selectively install what needs to be install.
+		names_to_install = [ x for x in R_dependency_list if not rpackages.isinstalled(x) ]
+		if len(names_to_install) > 0:
+			utils.install_packages(StrVector(names_to_install))
 
-	# Selectively install what needs to be install.
-	names_to_install = [ x for x in R_dependency_list if not rpackages.isinstalled(x) ]
-	if len(names_to_install) > 0:
-		utils.install_packages(StrVector(names_to_install))
-
-	utils.install_packages('Darts_BHT/Darts_0.1.0.tar.gz')
+		utils.install_packages('Darts_BHT/Darts_0.1.0.tar.gz')
 	return
 
 if __name__ == '__main__':
